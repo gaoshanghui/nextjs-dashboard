b252b1ead5e8833c0049ca24e98a249907be9575
Shanghui Gao <shanghui.g.gao@gmail.com>
2026-03-10 20:41:10 +0900

    Completed the Course

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,7 +4,13 @@ import { lusitana } from '@/app/ui/fonts';
 import CardWrapper from '@/app/ui/dashboard/cards';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
- 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+}; 
+
+
 export default async function Page() {
   return (
     <main>
